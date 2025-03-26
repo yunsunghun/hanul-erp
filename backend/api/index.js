b908c3eaea02015968c574ba75119d69df50bb72
sunghoon711 <sunghoon824711@gamil.com>
2025-03-26 12:04:12 +0900

    backend -api handler

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,34 +1,35 @@
 import { MongoClient } from "mongodb";
-
 export default async function handler(req, res) {
-  const client = new MongoClient(process.env.MONGODB_URI);
+  // 몽고db 클라이언트 URL
+  const client = new MongoClient(process.env.MONGOBD_URL);
+  //몽고 DB 클라이언트 연결 수립
+  await client.connect();
+  const db = client.db("hr");
+  const coll = db.collection("employees");
+
+  //GET 요청 or POST 요청 인지에 따라서로다른 데이터베이스 요청 처리
 
   try {
-    console.log("Request method:", req.method);
-    console.log("Request body:", req.body);
-    await client.connect();
-    const db = client.db("hr");
-    const employees = db.collection("employees");
-
     switch (req.method) {
-      case "GET":
-        const allEmployees = await employees.find({}).toArray();
+      case 'GET':
+        const allEmployees = await coll.find().toArray()
         res.status(200).json(allEmployees);
         break;
-      case "POST":
-        const newEmployee = req.body;
-        const result = await employees.insertOne(newEmployee);
-        res.status(201).json(result);
+      case 'Post':
+        const newEmployee = await coll.insertOne(newEmployee);
+        res.status(200).json(result);
         break;
       default:
-        res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
-        res.status(405).end(`Method ${req.method} Not Allowed!`);
+        res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+        res.status(405).end('${req.method}는 허용되지않습니다!')
     }
   } catch (e) {
-    console.error("Detailed Error:", e);
+    console.log(e);
     res.status(500).json({
       error: e.message,
-      stack: e.stack,
-    });
+      msg: '서버 통신 오류, 관리자 문의 요망!'
+    })
+  } finally {
+    await client.close();
   }
 }
